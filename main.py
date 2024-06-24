b6c2e1311212973d13b832496d4dc592c97546d4
as-budi <agungsetiabudi@ub.ac.id>
2024-06-24 15:56:14 +0700

    add some collors

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import neopixel
 from machine import Pin
 import time
 
-ws_pin = 23
+ws_pin = 16
 led_num = 1
 BRIGHTNESS = 0.1  # Adjust the brightness (0.0 - 1.0)
+
+red = (255, 0, 0)
+orange = (255, 50, 0)
+yellow = (255, 100, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+indigo = (100, 0, 90)
+violet = (200, 0, 100)
+
+colors = [red, orange, yellow, green, blue, indigo, violet]
 
 neoRing = neopixel.NeoPixel(Pin(ws_pin), led_num)
 
@@ -16,34 +26,32 @@ def set_brightness(color):
     return (r, g, b)
 
 def loop():
-    r = 0
-    while (r < 256):
-        g = 0
-        while (g < 256):
-            b = 0
-            while (b < 256):          
-                color = (r, g, b)  # Red color
-                color = set_brightness(color)
-                neoRing.fill(color)
-                neoRing.write()
-                time.sleep(0.1)
-                b = b + 10
-            g = g + 10
-        r = r + 10
-
-    # Display green
-    #color = (0, 255, 0)  # Green color
-    #color = set_brightness(color)
-    #neoRing.fill(color)
-    #neoRing.write()
-    #time.sleep(1)
-
-    # Display blue
-    #color = (0, 0, 255)  # Blue color
-    #color = set_brightness(color)
-    #neoRing.fill(color)
-    #neoRing.write()
-    #time.sleep(1)
+    for color in colors:
+        color = set_brightness(color)
+        neoRing.fill(color)
+        neoRing.write()
+        time.sleep(1)
+    
+#     # Display green
+#     color = (255, 0, 0)  # Green color
+#     color = set_brightness(color)
+#     neoRing.fill(color)
+#     neoRing.write()
+#     time.sleep(1)
+# 
+#     # Display green
+#     color = (0, 255, 0)  # Green color
+#     color = set_brightness(color)
+#     neoRing.fill(color)
+#     neoRing.write()
+#     time.sleep(1)
+# 
+#     # Display blue
+#     color = (0, 0, 255)  # Blue color
+#     color = set_brightness(color)
+#     neoRing.fill(color)
+#     neoRing.write()
+#     time.sleep(1)
 
 while True:
     loop()
